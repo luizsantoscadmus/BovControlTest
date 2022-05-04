8e989bfdff0e01409ccd437ce7b3ec48d358c511
Luiz Felipe <luiz.santos@cadmus.com.br>
2022-05-04 11:59:19 -0300

    feat: Funcionalidades da Home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,18 +1,90 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {View, Button} from 'react-native';
+import React, {useMemo, useEffect} from 'react';
+import {Button, FlatList} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+import Toast from 'react-native-toast-message';
+
+import List from '../../components/List';
+import ChecklistContext from '../../store/realm';
+import Checklist, {IChecklist} from '../../store/realm/schema';
+const {useQuery, useRealm} = ChecklistContext;
+
+import Styles from './Home.styles';
+import useFetch from '../../hooks/useFetch';
+import saveDataInLocal from '../../services/saveDataInLocal';
+import {BASE_URL} from '../../services/utils';
 
 function HomeScreen() {
   const navigation = useNavigation<any>();
+  const result = useQuery(Checklist);
+  const checklists = useMemo(() => result.sorted('created_at', true), [result]);
+  const realm = useRealm();
 
-  const onPress = () => {
-    navigation.navigate('Item');
+  const {makeRequest, data, isLoading} = useFetch<IChecklist>(
+    `${BASE_URL}checklists`,
+  );
+
+  const onPress = (item: IChecklist) => {
+    navigation.navigate('Item', {id: item.id});
   };
 
+  const renderItem = ({item}: any) => (
+    <List.Item item={item} onPress={onPress} />
+  );
+
+  NetInfo.addEventListener(state => {
+    // TODO: sync data when network goes online
+    if (!state.isConnected) {
+      Toast.show({
+        type: 'error',
+        text1: 'Sem conexão',
+        text2:
+          'Os dados serão salvos localmente e sincronizados quando houver conexão',
+      });
+    }
+  });
+
+  useEffect(() => {
+    makeRequest();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      saveDataInLocal(realm, data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <Styles.EmptyMessageContainer>
+        <Styles.EmptyMessage>Carregando...</Styles.EmptyMessage>
+      </Styles.EmptyMessageContainer>
+    );
+  }
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={onPress} title="Ir para a tela de Item" />
-    </View>
+    <Styles.SafeArea>
+      {checklists.length === 0 && (
+        <Styles.EmptyMessageContainer>
+          <Styles.EmptyMessage>Nenhum item encontrado</Styles.EmptyMessage>
+          <Button
+            onPress={() => navigation.navigate('Item')}
+            title="Cadastrar item"
+          />
+        </Styles.EmptyMessageContainer>
+      )}
+      {checklists.length > 0 && (
+        <FlatList
+          // eslint-disable-next-line react-native/no-inline-styles
+          contentContainerStyle={{paddingTop: 20}}
+          data={checklists}
+          renderItem={renderItem}
+          keyExtractor={(item: any) => item.id}
+        />
+      )}
+    </Styles.SafeArea>
   );
 }
 
