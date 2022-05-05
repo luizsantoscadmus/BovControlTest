9a0200752f95bf497b12ef2799b84118d4eda2f9
Luiz Felipe <luiz.santos@cadmus.com.br>
2022-05-04 22:15:10 -0300

    chore: Adiciona componentes Select e Switch nos Forms

---
+++ src/components/Form/Switch/Switch.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Switch as RNSwitch, SwitchProps} from 'react-native';
+
+function Switch(props: SwitchProps) {
+  return <RNSwitch {...props} />;
+}
+
+export default Switch;
--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,9 +1,13 @@
 import Field from './Field';
 import Label from './Label';
 import Input from './Input';
+import Select from './Select';
+import Switch from './Switch';
 
 export default {
   Field,
   Label,
   Input,
+  Select,
+  Switch,
 };
