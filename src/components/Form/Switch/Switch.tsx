7fd68078c83a8175f1c900ee3e2ccc7541b60b87
Luiz Felipe <luiz.santos@cadmus.com.br>
2022-05-05 07:33:15 -0300

    fix: Correção no alinhamento do Switch (Android)

--- src/components/Form/Switch/Switch.tsx
+++ src/components/Form/Switch/Switch.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {Switch as RNSwitch, SwitchProps} from 'react-native';
 
 function Switch(props: SwitchProps) {
-  return <RNSwitch {...props} />;
+  // eslint-disable-next-line react-native/no-inline-styles
+  return <RNSwitch style={{alignSelf: 'flex-start'}} {...props} />;
 }
 
 export default Switch;
